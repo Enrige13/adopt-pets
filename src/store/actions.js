cc83677ab0cb085978a929f02565a3bf716dfc8f
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-04-23 00:57:16 +0200

    a nice little vue project

---
+++ src/store/actions.js
@@ -0,0 +1,5 @@
+export default {
+  addPet: ({ commit }, payloard) => {
+    commit('appendPet', payloard)
+  }
+}
---
+++ src/views/Cats.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <PetTable
+          species="cats"
+          :pets="cats"
+        /> <!-- oder pet-table -->
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import PetTable from '@/components/PetTable.vue'
+
+export default {
+  components: {
+    PetTable
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
